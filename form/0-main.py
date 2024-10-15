c425e0180a8d2458d11e567218d93db67ab72a98
Suntha <Suntha.Lucas@atlasschool.com>
2024-10-14 22:14:27 -0500

    0-main

---
+++ form/0-main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import xml.etree.ElementTree as ET
+
+def main():
+    """
+    Check if the post-comments section is a sibling of the post div.
+    """
+
+    tree = ET.parse(sys.argv[1])
+    root = tree.getroot()
+    body = root.find('.//body')
+
+    post_div = body.find('.//div[@class="post"]')
+    post_comments_section = body.find('.//section[@class="post-comments"]')
+
+    if post_comments_section is not None and post_div is not None:
+        # Check if they have the same parent (which is the body)
+        if post_comments_section.getparent() == post_div.getparent():
+            print('post-comments section is sibling to post div: True')
+        else:
+            print('post-comments section is sibling to post div: False')
+    else:
+        print('post-comments section is sibling to post div: False')
+
+if __name__ == '__main__':
+    main()
